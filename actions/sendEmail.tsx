1aa71f44e71fb0d8ce807e3cf108fb17b724db2c
Sam Fisher <sammee1@gmail.com>
2024-01-07 20:34:13 -0600

    email error handling

--- actions/sendEmail.tsx
+++ actions/sendEmail.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { validateString } from "@/lib/utils";
+import { validateString, getErrorMessage } from "@/lib/utils";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -22,11 +22,17 @@ export async function sendEmail(formData: FormData) {
     };
   }
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "samanthajfisher.cs@gmail.com",
-    subject: "Message from contact form",
-    reply_to: senderEmail as string,
-    text: senderMessage as string,
-  });
+  try {
+    resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "samanthajfisher.cs@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail as string,
+      text: senderMessage as string,
+    });
+  } catch (error) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 }
