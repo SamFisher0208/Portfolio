4f3313a0c71a89b29591dfab324512d7e1dca5a1
Sam Fisher <sammee1@gmail.com>
2024-01-08 21:05:24 -0600

    responsive styles

--- components/project.tsx
+++ components/project.tsx
@@ -30,8 +30,8 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="dark:bg-white/10 dark:hover:bg-white/20 bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition rounded-lg">
-        <div className="flex flex-col py-4 px-5 pb-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full group-even:ml-[18rem]">
+      <section className="dark:bg-white/10 dark:hover:bg-white/20 bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-200 transition rounded-lg">
+        <div className="flex flex-col py-4 px-5 pb-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="dark:text-white/70 mt-2 leading-relaxed text-gray-700">
             {description}
@@ -59,7 +59,7 @@ export default function Project({
         group-even:group-hover:rotate-2
         
         group-hover:scale-[1.04] 
-        transition"
+        transition hidden sm:block"
           src={imageUrl}
           alt="Projects I worked on"
           quality={95}
